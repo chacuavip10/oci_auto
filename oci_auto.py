12824e883cc4cb4415cff1fda45c20806b2eb2fa
cstanfield1 <stanfieldcory@yahoo.com>
2022-09-12 21:15:40 -0700

    modified:   oci_auto.py Added IFTTT, Config Tool, Typo/Security fixes

--- oci_auto.py
+++ oci_auto.py
@@ -3,6 +3,14 @@ import logging
 import time
 import sys
 import requests
+import pathlib
+import json
+
+from pickle import TRUE
+from pathlib import Path
+from getpass import getpass
+
+#########################################      Log Settings        #####################################################
 
 LOG_FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
 logging.basicConfig(
@@ -14,21 +22,67 @@ logging.basicConfig(
     ]
 )
 
-#####################################      SCRIPT SETTING, CHANGE THIS        #########################################
+#####################################      SCRIPT SETTINGS       #######################################################
 # Script Setting, change this (Each VM.Standard.A1.Flex ocpus = 6 GB memroy)
 # instance_type = "VM.Standard.A1.Flex"
 # OCI API_KEY and other parametter: https://github.com/hitrov/oci-arm-host-capacity (intercept request from web console)
-ocpus = 4
-memory_in_gbs = ocpus*6
-wait_s_for_retry = 30
-instance_display_name = 'ubuntu-sg-oci*********'
-compartment_id = 'ocid1.tenancy.oc1..**********************************'
-domain = "rhNU:AP-********************"  # availability_domain
-image_id = "ocid1.image.oc1.ap-singapore-1.*******************************"
-subnet_id = 'ocid1.subnet.oc1.ap-singapore-1.********************'
 
-ssh_key = "ssh-rsa ************************"
-# Telegram setting
+ocpus = 4 #Configurable
+memory_in_gbs = ocpus*6 #Configurable
+wait_s_for_retry = 30 #Configurable
+
+instance_size = f"{ocpus} ocpus and {memory_in_gbs} GB of memory"
+path = Path.cwd() #current dir
+
+#################################    CONFIG GENERATION AND VARIABLE HANDLER     ########################################
+
+config_loop = True
+while config_loop:
+    finstance_display_name = 'displayName'
+    fcompartment_id = 'compartmentId'
+    fdomain = "availabilityDomain"
+    fimage_id = "imageId"
+    fsubnet_id = 'subnetId'
+    fssh_key = 'ssh_authorized_keys'
+
+    post_data = input(f"To generate config paste POST data output here:(to intercept request from web console: Submit instance request in OCI -> Settings -> Tools -> Network -> Find 500 error -> Copy -> POST data)' \n")
+    script_settings = json.loads(post_data)
+    print("\nParsing Request data and generating instance configuration")
+    time.sleep(3)
+    if finstance_display_name in script_settings:
+        instance_display_name = script_settings[finstance_display_name]
+        print(f"Display Name: {instance_display_name}")
+        time.sleep(1)
+    if fdomain in script_settings:
+        domain = script_settings[fdomain]
+        print(f"Domain: {domain}")
+        time.sleep(1)
+    if fcompartment_id in script_settings:
+        compartment_id = script_settings[fcompartment_id]
+        print(f"Compartment ID: {compartment_id}")
+        time.sleep(1)
+    if fimage_id in script_settings['sourceDetails']:
+        print(f"Image ID: ", script_settings['sourceDetails'][fimage_id])
+        image_id = script_settings['sourceDetails'][fimage_id]
+        time.sleep(1)
+    if fsubnet_id in script_settings['createVnicDetails']:
+        print(f"Subnet ID: ", script_settings['createVnicDetails'][fsubnet_id])
+        subnet_id = script_settings['createVnicDetails'][fsubnet_id]
+        time.sleep(1)
+    if fssh_key in script_settings['metadata']:
+        ssh_key = script_settings['metadata'][fssh_key]
+        print(f"SSH key is ****** {ssh_key[-18:]}")
+
+    cont = input(f"Would you like to continue with this configuration Y|N? \n").lower()
+    if "no" in cont or "n" in cont:
+        print("Existing configuration will be deleted and you will be returned to generate a new configuration file")
+        continue
+    else:        
+        print(f"Continuing with instance configuration for {instance_display_name}")
+        time.sleep(2)
+        break
+
+######################################## Telegram setting ############################################################
 # https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
 # Create bot with BotFather, get the API key
 # Start the bot via conversation/chat with command /start
@@ -48,6 +102,17 @@ def telegram_notify(session, bot_api, chat_id, message):
         logging.info("Message fail to sent via telegram")
 
 #######################################################################################################################
+
+#####################################      IFTTT SETTINGS         ######################################################
+# Create new action from Webhook "Recieve WebRequest"
+# From Webhook documentation copy the example URL
+# Use variables from Script settings in web request
+
+url = "" # maker.ifttt.com/trigger/recipe_name/with/key/your_key
+value1 = instance_display_name
+value2 = domain
+value3 = instance_size
+
 logging.info("#####################################################")
 logging.info("Script to spawn VM.Standard.A1.Flex instance")
 
@@ -58,7 +123,7 @@ telegram_notify(session, bot_api, chat_id, message)
 
 # Loading config file
 logging.info("Loading OCI config")
-config = oci.config.from_file(file_location="./config")
+config = oci.config.from_file(file_location = f"{Path.cwd()}\\config")
 
 # Initialize service client
 logging.info("Initialize service client with default config file")
@@ -92,7 +157,6 @@ if response:
 
     message = f"Current: {_A1_Flex} active VM.Standard.A1.Flex instance(s) (including RUNNING OR STOPPED)"
     logging.info(message)
-    telegram_notify(session, bot_api, chat_id, message)
 else:
     logging.info(f"No instance(s) found!")
 
@@ -106,17 +170,15 @@ telegram_notify(session, bot_api, chat_id, message)
 if total_ocpus + ocpus > 4 or total_memory + memory_in_gbs > 24:
     message = "Total maximum resource exceed free tier limit (Over 4 ocpus/24GB total). **SCRIPT STOPPED**"
     logging.critical(message)
-    telegram_notify(session, bot_api, chat_id, message)
     sys.exit()
 
 # Check for duplicate display name
 if instance_display_name in instance_names:
     message = f"Duplicate display name: >>>{instance_display_name}<<< Change this! **SCRIPT STOPPED**"
     logging.critical(message)
-    telegram_notify(session, bot_api, chat_id, message)
     sys.exit()
 
-message = f"Precheck pass! Create new instance VM.Standard.A1.Flex: {ocpus} opus - {memory_in_gbs} GB"
+message = f"Precheck pass! Create new instance VM.Standard.A1.Flex: {ocpus} ocpus - {memory_in_gbs} GB"
 logging.info(message)
 telegram_notify(session, bot_api, chat_id, message)
 ######################################################################################################################
@@ -160,13 +222,18 @@ while to_try:
         message = 'Success! Edit vnic to get public ip address'
         logging.info(message)
         telegram_notify(session, bot_api, chat_id, message)
+        # post to IFTTT
+        requests.post(f"{url}?value1={value1}&value2={value2}&value3={value3}")
         # print(to_launch_instance.data)
         session.close()
     except oci.exceptions.ServiceError as e:
         if e.status == 500:
             # Out of host capacity.
             message = f"{e.message} Retry in {wait_s_for_retry}s"
-            #telegram_notify(session, bot_api, chat_id, message)
+        elif e.status == 429:
+            # TooManyRequests
+            message = f"target_service: compute, status: 429, code: TooManyRequests Retry in {wait_s_for_retry}s"
+            telegram_notify(session, bot_api, chat_id, message)
         else:
             message = f"{e} Retry in {wait_s_for_retry}s"
             telegram_notify(session, bot_api, chat_id, message)
